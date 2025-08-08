0583b38015b839d7d27fcd47ea1501e8ede70528
Noah-Ram52 <merawe481@gmail.com>
2025-08-08 16:42:59 -0500

    12.4.6

--- models/clothingItems.js
+++ models/clothingItems.js
@@ -23,6 +23,23 @@ const clothingItem = new mongoose.Schema({
       message: "Link is not valid",
     },
   },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+clothingItem.path("likes").default([]);
 
 module.exports = mongoose.model("clothingItems", clothingItem);
