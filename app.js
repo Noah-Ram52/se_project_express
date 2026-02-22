2a76ed3052fc83c547c7dae627543ad7d291836d
Noah-Ram52 <merawe481@gmail.com>
2026-02-22 17:06:25 -0600

    Editing README.md & Testing Server Errors

--- app.js
+++ app.js
@@ -26,6 +26,16 @@ app.use(cors());
 app.use(express.json());
 
 app.use(requestLogger); // Request logger goes first
+
+// Add the crash test route here (before your main routes)
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
+// The next line is /signup & /signup route handlers
+
 app.use("/", mainRouter); // routes (this is your "routes")
 
 app.use(errorLogger); // enabling the error logger
