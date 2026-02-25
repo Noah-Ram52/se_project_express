b734938959e836a4d1ea8a8e142a0e2e7844f73c
Noah-Ram52 <merawe481@gmail.com>
2026-02-25 15:05:59 -0600

    Removing unneccsary backend info

--- app.js
+++ app.js
@@ -27,13 +27,6 @@ app.use(express.json());
 
 app.use(requestLogger); // Request logger goes first
 
-// Add the crash test route here (before your main routes)
-app.get("/crash-test", () => {
-  setTimeout(() => {
-    throw new Error("Server will crash now");
-  }, 0);
-});
-
 // The next line is /signup & /signup route handlers
 
 app.use("/", mainRouter); // routes (this is your "routes")
